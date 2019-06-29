6989096ace5b49d033dc235fbc4f02485cc27030
whatasoda <a16dc107@dhw.ac.jp>
2019-06-29 14:25:04 +0900

    Updates

---
+++ types/assets/index.d.ts
@@ -0,0 +1,29 @@
+declare module '*.scss' {
+  const ClassNameRecord: Record<string, string>;
+  export default ClassNameRecord;
+}
+
+declare module '*.jpg' {
+  const publicPath: string;
+  export default publicPath;
+}
+
+declare module '*.png' {
+  const publicPath: string;
+  export default publicPath;
+}
+
+declare module '*.gif' {
+  const publicPath: string;
+  export default publicPath;
+}
+
+declare module '*.webp' {
+  const publicPath: string;
+  export default publicPath;
+}
+
+declare module '*.svg' {
+  const publicPath: string;
+  export default publicPath;
+}
